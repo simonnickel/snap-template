7fea7c496863c8decc824517287e7bd72fbf1bb1
Simon Nickel <simonnickel@googlemail.com>
2025-08-16 21:00:56 +0200

    Remove SnapTheme from ExampleScreen

--- SnapTemplateDemo/SnapTemplateDemo/Screens/ExampleScreen.swift
+++ SnapTemplateDemo/SnapTemplateDemo/Screens/ExampleScreen.swift
@@ -6,7 +6,7 @@
 import SnapDependencies
 import SnapNavigation
 import SnapTemplateUtil
-import SnapTheme
+import SnapStyle
 import SwiftUI
 
 // TODO: See ToolbarButtonSettings below
@@ -14,50 +14,51 @@ import SnapTemplateSettings
 
 struct ExampleScreen: View {
 	
-	@Environment(\.theme) private var theme
 	@Dependency(\.dataSource) private var dataSource
     @Environment(\.navigator) private var navigator
+    @Environment(\.isPresentingDestination) private var isPresentingDestination
 	
 	let destination: AppDestination
 	
 	var body: some View {
 		
-        ThemeScreen(style: .plain) {
-            
-            List {
+        StyleList {
 
-                Section {
-                    
+            Section {
+                
+                StyleListRow(.plain) {
                     Text(dataSource.content)
-                        .themeListRow()
-                    
-                    NavigationListRow(destination: AppDestination.rectangle)
-                    
-                } header: {
-                    
-                    Text(destination.definition.title + " Screen")
-                        .theme(font: .listHeader)
-                    
-                } footer: {
-                    
-                    ThemeVStack(spacing: .spacingElements) {
-                        Button(action: {
-                            navigator(.present(AppDestination.circle))
-                        }, label: {
-                            ThemeLabel(text: "Push .circle", style: .themeButtonPrimary)
-                        })
-                        .buttonStyle(.themePrimary)
-                        
-                        // TODO: Should be part of the template and in the navigationbar.
-                        ToolbarButtonSettings()
-                    }
-                    .theme(padding: .spacingGroups, [.vertical, .trailing])
-                    .frame(maxWidth: .infinity)
-                    
                 }
-	
-			}
-	
+                
+                // TODO: isPresentingDestination does not work well here, because the same list row exists on the next screen. Could be changed to `isPresenting(_ destination:, from:)` and check the combination.
+                // TODO: , systemImage: AppDestination.rectangle.definition.icon
+                // TODO: isPresented might  just get a bool instead of a closure.
+                StyleListRow(.navigate(AppDestination.rectangle, isPresented: { value in isPresentingDestination(value) })) {
+                    Text(AppDestination.rectangle.definition.title)
+                }
+                
+            } header: {
+                
+                Text(destination.definition.title + " Screen")
+                    .styleListSectionHeaderLabel()
+                
+            } footer: {
+                
+                StyleStack(spacing: \.spacingElements, alignmentH: .center, isStretching: false) {
+                    StyleButton {
+                        navigator(.present(AppDestination.circle))
+                    } content: {
+                        Text("Push .circle")
+                    }
+                    
+                    // TODO: Should be part of the template and in the navigationbar.
+                    ToolbarButtonSettings()
+                }
+                .style(padding: \.spacingGroups, [.vertical, .trailing])
+                .stretch(alignment: .center)
+                
+            }
+
 		}
 		
 	}
