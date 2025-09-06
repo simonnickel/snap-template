1b79c0788443ac0c30081e83768c1a8dfeea1b04
Simon Nickel <simonnickel@googlemail.com>
2025-09-06 21:43:58 +0200

    Simplify toolbar button styling

--- Sources/SnapTemplateUtil/Template+ToolbarItem.swift
+++ Sources/SnapTemplateUtil/Template+ToolbarItem.swift
@@ -13,38 +13,9 @@ extension Template {
     @ToolbarContentBuilder
     public static func toolbarItem(_ destination: SnapNavigationDestination, placement: ToolbarItemPlacement = .automatic) -> some ToolbarContent {
         ToolbarItem(placement: placement) {
-            NavigationToolbarButton(destination: destination)
-        }
-    }
-}
-
-private struct NavigationToolbarButton: View {
-    
-    @Environment(\.navigator) private var navigator
-    
-    private let destination: SnapNavigationDestination
-    
-    internal init(destination: SnapNavigationDestination) {
-        self.destination = destination
-    }
-    
-    internal var body: some View {
-        StyleButton(.plain) {
-            navigator(.present(destination))
-        } content: {
-            // TODO: StyleLabel shows weird spacing, because it does not support icon-only. As soon as there is an icon, SwiftUI enforces an icon-only button style.
-            // Would be possible if StyleLabel supports .iconOnly or a similar mechanism.
-            // Would make NavigationToolbarButton redundant, could use NavigationButton.
-            // AnyView(destination.label)
-            Label {
-                Text(destination.definition.title)
-            } icon: {
-                if let icon = destination.icon {
-                    StyleIcon(icon)
-                } else {
-                    EmptyView()
-                }
-            }
+            NavigationButton(destination: destination)
+                .style(labelContent: [.icon])
+                .style(buttonVariant: .plain)
         }
     }
 }
